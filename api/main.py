13b45f514fd1e37f558510a41fcecdade3153f65
Mateus Gutierrez <118942632+MateusGutierrez@users.noreply.github.com>
2025-03-05 10:10:21 -0300

    add cors

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes.user_routes import user_bp, list_bp
 
 app = Flask(__name__)
+
+# Liberar CORS para todas as rotas e origens
+CORS(app)
 
 # Registrar blueprints (rotas)
 app.register_blueprint(user_bp, url_prefix="/user")
